bf230893b884130daefe44faa825eca7feaccb74
ovilia-afk <ovisucira@gmail.com>
2026-03-16 06:24:01 +0700

    resolve merge conflict

--- Pertemuan4/src/MBangunDatar.java
+++ Pertemuan4/src/MBangunDatar.java
@@ -1,9 +1,5 @@
 /* Nama File    : MBangunDatar.java
-<<<<<<< Pertemuan4Latihan
- * Deskripsi    : berisi aplikasi method dalam class BangunDatar
-=======
  * Deskripsi    : berisi aplikasi method dalam class MBangunDatar
->>>>>>> local
  * Pembuat      : Ovilia Suci Ramadhani / 24060124120040
  * Tanggal      : 11 Maret 2026
  */
@@ -26,3 +22,39 @@ public class MBangunDatar {
         BangunDatar.printCounterBangunDatar(); // mencetak jumlah bangun datar yang telah dibuat
     }
 }
+
+// /* Nama File    : Dosen.java
+//  * Deskripsi    : berisi atribut dan method dalam class Dosen
+//  * Pembuat      : Ovilia Suci Ramadhani / 24060124120040
+//  * Tanggal      : 15 Maret 2026
+//  */
+
+// import java.time.LocalDate;
+
+// public class Dosen extends Pegawai {
+//     // Atribut
+//     protected String fakultas;
+
+//     // Method
+
+//     // konstruktor tanpa parameter
+//     public Dosen(){
+
+//     }
+    
+//     // konstruktor dengan parameter nip, nama, tanggalLahir, tmt, gajiPokok dan fakultas
+//     public Dosen(String nip, String nama, LocalDate tanggalLahir, LocalDate tmt, double gajiPokok, String fakultas){
+//         super(nip, nama, tanggalLahir, tmt, gajiPokok);
+//         this.fakultas = fakultas;
+//     }
+
+//     // mengembalikan nilai fakultas
+//     public String getFakultas(){
+//         return this.fakultas;
+//     }
+
+//     // mengeset nilai fakultas
+//     public void setFakultas(String fakultas){
+//         this.fakultas = fakultas;
+//     }
+// }
